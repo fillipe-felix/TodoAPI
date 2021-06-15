ae7a65d2b4d60d25210e292f3d958056adbb1174
fillipe.felix <felipesoares_1993@hotmail.com>
2021-06-15 14:49:43 -0300

    first commit, todoList microsoft

---
+++ API.onboarding/Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.onboarding.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
